7aee5ff04eeee614e0d51b5eda465dd0bf684b85
Wilmon Monteros <wilmonM@SourcefitMNL.com>
2018-11-22 21:35:36 +0800

    nov 22

--- js/move_element.js
+++ js/move_element.js
@@ -10,8 +10,8 @@ $(function() {
   $(".blue-box").animate({
   	//java objects here
   	"margin-left" : "-=200px"
-  	//linear - 2px ever 5 ms (200px/1000)
-  	//default is swing
+    //linear - 2px ever 5 ms (200px/1000)
+    //default is swing
 
   }, 1000, "linear" );
 
